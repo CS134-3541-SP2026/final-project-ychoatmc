ea49f7b5857bde3f400e4a2bade6b926beec99c6
Yuni <chossn@yahoo.com>
2026-05-15 17:10:34 -0700

    List items with checkboxes

--- app/src/main/java/com/example/pricecheckshoppinglist/views/ListScreen.kt
+++ app/src/main/java/com/example/pricecheckshoppinglist/views/ListScreen.kt
@@ -1,19 +1,27 @@
 package com.example.pricecheckshoppinglist.views
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.pricecheckshoppinglist.viewModels.ItemViewModel
@@ -44,11 +52,32 @@ fun ListScreen(
         Column(modifier = Modifier.weight(.9f)) {
             if (viewModel.itemCount() != 0)
                 viewModel.allItems.collectAsState().value.forEach {
-                    Row {
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        var textColor by remember { mutableStateOf(Color.Black) }
+                        var isChecked by remember { mutableStateOf(false) }
+                        Checkbox(checked = isChecked, onCheckedChange = {
+                            isChecked = it
+                            if(isChecked) {
+                                textColor = Color.LightGray
+                            }
+                            else{
+                                textColor = Color.Black
+                            }
+                            })
                         TextButton({ onEditItemPageClick(it.name, storeName) }) {
-                            Text(it.name)
+                            Text(it.name, color = textColor)
                         }
-                        Text(viewModel.getPrice(it.name, storeName).toString())
+                        Text(viewModel.getQuantity(it.name, storeName).toString() + " " +
+                        viewModel.getUnit(it.name, storeName),
+                            textAlign = TextAlign.Right,
+                            color = textColor,
+                            modifier = Modifier.width(200.dp))
+                        Text(viewModel.getPrice(it.name, storeName).toString(),
+                            textAlign = TextAlign.Right,
+                            color = textColor,
+                            modifier = Modifier.width(300.dp))
                     }
                 }
         }
